fe0d577849c1acfc09e74ef481e0ec466f462db7
turgayh <email@example.com>
2019-03-27 00:10:32 +0300

    update test code

--- src/test/scala/basic/first.scala
+++ src/test/scala/basic/first.scala
@@ -11,7 +11,6 @@ class first extends Simulation {
       .acceptLanguageHeader("en-US,en;q=0.5")
       .acceptEncodingHeader("gzip, deflate")
       .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
-      ///.proxy(Proxy("proxy.intertech.com.tr", 8080))
   val scn: ScenarioBuilder = scenario("HomePageSimulation")
     .exec(http("Home page request")
       .get("/"))
